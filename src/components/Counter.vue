99dfbfe8d13ce781e8b9a714434f06d014a735fd
Moritz Zimmermann <nguyendinhnhattai.work@gmail.com>
2024-07-15 02:00:26 +1000

    Something typescript learning and testing stuff out

---
+++ src/components/Counter.vue
@@ -0,0 +1,22 @@
+<script setup lang="ts">
+import { ref } from 'vue';
+
+const props = defineProps<{
+  msg: int
+}>();
+
+const count = ref<number>(0);
+
+const increment = () => {
+  count++;
+};
+
+</script>
+
+<template>
+  <div class="counter">
+    <h1>Counter</h1>
+    <p>Count : {{ count }}</p>
+    <button v-on:click="increment"></button>
+  </div>
+</template>
